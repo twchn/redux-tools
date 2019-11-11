5539a177124fef60decbdf8e2afb1614819ae197
Winrey <i@iaside.com>
2019-11-11 13:43:07 +0800

    🤔refactor: make api more comfortable

--- src/createActions.ts
+++ src/createActions.ts
@@ -1,4 +1,4 @@
-import { IActions, IActionTypes } from './typings';
+import { IActions, IActionTypes, IActionsTypes } from './typings';
 import createAction from './createAction';
 
 /**
@@ -6,18 +6,19 @@ import createAction from './createAction';
  * @param {IActionTypes} actionTypes
  * @returns {IActions<any>}
  */
-function createActions<T extends IActions<any> = IActions<any>>(actionTypes: IActionTypes): T {
-  if (typeof actionTypes !== 'object' || actionTypes == null) {
-    throw new Error('Action types must be an object!');
-  }
-  const types = Object.keys(actionTypes);
-  if (types.length === 0) {
-    throw new Error('Action types must be not empty!');
-  }
-  return types.reduce((acc: IActions<any>, type: string) => {
-    acc[type.toLowerCase().replace(/([-_]\w)/g, g => g[1].toUpperCase())] = createAction(actionTypes[type]);
-    return acc;
-  }, {}) as unknown as T;
+function createActions<T extends object = {}>(actionTypes: IActionTypes)
+  : ({} extends T ? IActions<any> : IActionsTypes<T>) {
+    if (typeof actionTypes !== 'object' || actionTypes == null) {
+      throw new Error('Action types must be an object!');
+    }
+    const types = Object.keys(actionTypes);
+    if (types.length === 0) {
+      throw new Error('Action types must be not empty!');
+    }
+    return types.reduce((acc: IActions<any>, type: string) => {
+      acc[type.toLowerCase().replace(/([-_]\w)/g, g => g[1].toUpperCase())] = createAction(actionTypes[type]);
+      return acc;
+    }, {}) as unknown as ({} extends T ? IActions<any> : IActionsTypes<T>);
 }
 
 export default createActions;
--- test/createActions.test.ts
+++ test/createActions.test.ts
@@ -1,6 +1,4 @@
-import { IActionsTypes } from './../src/typings/index';
 import { createActionTypes, createActions } from '../src';
-import { IActionCreator } from '../src/typings';
 
 describe('test the generating actions function', () => {
   it('should return three default actions', () => {
@@ -37,29 +35,10 @@ describe('test the generating actions function', () => {
     const types = ['REQUEST', 'REQUEST_SUCCESS', 'REQUEST-FAILURE'];
     const actionTypes = createActionTypes('TYPE', types);
     const generatedActions = createActions<{
-      'request': IActionCreator<{ data: number }>,
-      'requestSuccess': IActionCreator<{ data: number }>,
-      'requestFailure': IActionCreator<{ data: number }>
-    }>(actionTypes);
-    const payload = { data: 2 };
-    const expectedActions = {
-      request: { type: actionTypes.REQUEST, payload },
-      requestSuccess: { type: actionTypes.REQUEST_SUCCESS, payload },
-      requestFailure: { type: actionTypes['REQUEST-FAILURE'], payload },
-    };
-
-    expect(generatedActions.request(payload)).toEqual(expectedActions.request);
-    expect(generatedActions.requestSuccess(payload)).toEqual(expectedActions.requestSuccess);
-  });
-
-  it('should return specified actions when use shorthand way to specify types on actions', () => {
-    const types = ['REQUEST', 'REQUEST_SUCCESS', 'REQUEST-FAILURE'];
-    const actionTypes = createActionTypes('TYPE', types);
-    const generatedActions = createActions<IActionsTypes<{
       'request': { data: number },
       'requestSuccess': { data: number },
       'requestFailure': { data: number }
-    }>>(actionTypes);
+    }>(actionTypes);
     const payload = { data: 2 };
     const expectedActions = {
       request: { type: actionTypes.REQUEST, payload },
